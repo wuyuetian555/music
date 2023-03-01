00d7f55d629e110fbc8f5aabfa0eb89b02a1f2a3
th <1210875592@qq.com>
2023-03-01 23:04:25 +0800

    尝试提交

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,22 @@
 const { defineConfig } = require("@vue/cli-service");
+const AutoImport = require("unplugin-auto-import/webpack");
+const Components = require("unplugin-vue-components/webpack");
+const { ElementPlusResolver } = require("unplugin-vue-components/resolvers");
 const path = require("path");
 
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-
+  configureWebpack: {
+    plugins: [
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+  },
   pluginOptions: {
     "style-resources-loader": {
       preProcessor: "less",
